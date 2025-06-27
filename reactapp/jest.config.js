33ab45c438f0254c39ceb4d27b2704f14db30e57
Badsena <sena@amypo.in>
2025-06-27 16:40:56 +0000

    User

---
+++ reactapp/jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  }
+};
